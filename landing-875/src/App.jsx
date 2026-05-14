47a863202e0e8a0a09c42c91e98e6d16db365bc2
BsttSntt0 <mauriciosnntt@gmail.com>
2026-05-14 01:57:10 -0300

    Atualiza ecossistema visual

--- landing-875/src/App.jsx
+++ landing-875/src/App.jsx
@@ -122,7 +122,7 @@ function Ecossistema() {
       {/* MOBILE (CIRCULAR) */}
       <div className="md:hidden relative w-full flex justify-center items-center">
         
-        <div className="relative w-[300px] h-[300px]">
+        <div className="relative w-75 h-75">
 
           {/* CENTRO */}
           <div className="absolute inset-0 flex flex-col items-center justify-center">
